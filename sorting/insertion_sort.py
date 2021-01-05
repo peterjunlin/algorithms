a47b287ad39c1d6dd21260784f59bbf5a0b33c47
Peter Lin <peterjunlin@gmail.com>
2021-01-04 19:39:45 -0800

    Test insertion_sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,28 @@
+def insertion_sort(lst):
+    l = len(lst)
+    i = 1
+    while i < l:
+        k = i
+        key = lst[i]
+        while (lst[k-1] > key) and (k>0):
+            lst[k] = lst[k-1]
+            k = k - 1
+        lst[k] = key
+        i += 1
+
+    return lst
+
+def test_insertion_sort(test_case):
+    result = insertion_sort(list(test_case))
+    print("------")
+    print(f"Result: {result}")
+    print(f"Expected: {sorted(test_case)}")
+    print(f"Success: {result == sorted(test_case)}")
+
+
+if __name__ == "__main__":
+    test_insertion_sort([])
+    test_insertion_sort([1])
+    test_insertion_sort([3, 1, 5, 8, 2])
+    test_insertion_sort([5, 4, 3, 2, 1, 0, -1, -2])
+    test_insertion_sort([-1, 0, 1, 2, 3, 4, 5, 6])
